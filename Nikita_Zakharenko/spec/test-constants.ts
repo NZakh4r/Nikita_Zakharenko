07c1b77da2beaf09d0ee6a283b62204f59e00391
Nikita Zakharenko <nikita.zakharenko@plus4u.net>
2023-01-06 18:37:36 +0200

    homework is done

---
+++ Nikita_Zakharenko/spec/test-constants.ts
@@ -0,0 +1,8 @@
+// add valid token before running tests
+export const accessToken =
+  "sl.BWaYV_VF3KXjHqvCuBGBVqa6HsvtNLs6WMALRU_xle7ifOlaw-OOGY6ylcpvc6XKxzsirp0JlcajMXAHSWNLMnvlF9_QAdUbQLyrmgfNrf8rovMIYruywwPQcG9Sl075pq9IY7k-0W_5";
+export const expiredAccessToken =
+  "sl.BWPV_nLriGIUiB0I3IeDAXvwA3SzbYnr9eOYM_tHIK5_YMa3rQmLFpkQdyKWVzJFy_vEigZPT1MMH88Dglyd7ZT4ykDaIwUV3jF5g0u0oEYZCwA6Ro1njSzEVY920X8EmkuigE0";
+export const dropboxPath = "/NZakH4Rt.txt";
+export const incorrectDropboxPath = "/false-NZakH4Rt.txt";
+export const fileName = "NZakH4Rt.txt";
